d5cb2938dac8a4bc5ad9e7a38e2b1dbfcf93da98
vlGoku <markolucic21@outlook.de>
2023-11-27 15:28:38 +0100

    added some functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import "./styles.scss";
 import { Gameboard } from "./modules/gameboard";
+import { Player } from "./modules/player";
+import { Ship } from "./modules/ship";
+
 
 let gameboardPlayer = new Gameboard();
 let gameboardKI = new Gameboard();
 
+const carrier = new Ship("carrier", 5, 5);
+const battleship = new Ship("battleship", 4, 4);
+const cruiser = new Ship("cruiser", 3, 3);
+const submarine = new Ship("submarine", 3, 2);
+const destroyer = new Ship("Destroyer", 2, 1); 
+
 gameboardPlayer.createGameboard();
 gameboardKI.createGameboard();
+
+gameboardPlayer.placeShip(battleship, 2, 3);
 
 console.log(gameboardPlayer.gameboard);
 console.log(gameboardKI.gameboard);
