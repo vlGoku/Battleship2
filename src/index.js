56e3fb30b26aba9f0c24739b61dbe2a31c6049e9
vlGoku <markolucic21@outlook.de>
2023-11-28 13:54:54 +0100

    fixed problem, functions for placing CPU Ships

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { Gameboard } from "./modules/gameboard";
 import { Player } from "./modules/player";
 import { Ship } from "./modules/ship";
 
-
 let gameboardPlayer = new Gameboard();
 let gameboardKI = new Gameboard();
 
@@ -11,12 +10,17 @@ const carrier = new Ship("carrier", 5, 5);
 const battleship = new Ship("battleship", 4, 4);
 const cruiser = new Ship("cruiser", 3, 3);
 const submarine = new Ship("submarine", 3, 2);
-const destroyer = new Ship("Destroyer", 2, 1); 
+const destroyer = new Ship("Destroyer", 2, 1);
 
 gameboardPlayer.createGameboard();
 gameboardKI.createGameboard();
 
 gameboardPlayer.placeShip(battleship, 2, 3);
+gameboardKI.placeShipCPU(carrier);
+gameboardKI.placeShipCPU(battleship);
+gameboardKI.placeShipCPU(cruiser);
+gameboardKI.placeShipCPU(submarine);
+gameboardKI.placeShipCPU(destroyer);
 
 console.log(gameboardPlayer.gameboard);
 console.log(gameboardKI.gameboard);
--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,6 +1,5 @@
 "use strict";
 
-import { SingleEntryPlugin } from "webpack";
 import { Ship } from "./ship";
 
 class Gameboard {
@@ -19,11 +18,10 @@ class Gameboard {
       }
     }
   }
-
-  placeShip(ship, x, y){
+  placeShip(ship, x, y) {
     let z = 1;
-    for(let i = 0; i < ship.shipLength; i++){
-      if([x + i] > 9) {
+    for (let i = 0; i < ship.shipLength(); i++) {
+      if ([x + i] > 9) {
         this.gameboard[x - z][y] = ship.shipNumber;
         z++;
       } else {
@@ -33,13 +31,31 @@ class Gameboard {
     this.ships.push(ship);
   }
 
+  createShipsCPU() {
+    const carrier = new Ship("carrier", 5, 5);
+    const battleship = new Ship("battleship", 4, 4);
+    const cruiser = new Ship("cruiser", 3, 3);
+    const submarine = new Ship("submarine", 3, 2);
+    const destroyer = new Ship("Destroyer", 2, 1);
+  }
+
   placeShipCPU(ship) {
     let x = parseInt(Math.random() * 10);
     let y = parseInt(Math.random() * 10);
-    this.placeShip(ship, x, y);
+    if (this.gameboard[x][y] === this.gameboard[0][0]) {
+      this.placeShip(ship, x, y);
+    } else {
+      this.placeShipCPU(ship);
+    }
   }
 
-  attackShip(x, y){
+  placeShipCPULoop() {
+    for (let i = 1; i < this.ships.length; i++) {
+      this.placeShipCPU(this.ships[i]);
+    }
+  }
+
+  attackShip(x, y) {
     const currentItem = this.gameboard[x][y];
     this.ships.forEach((ship) => {
       if (ship.shipNumber === currentItem) {
@@ -48,25 +64,25 @@ class Gameboard {
         this.checkShipSunk();
       }
     });
-    if(this.gameboard[x][y] === 0){
-      this.gameboard[x][y] = "X"
+    if (this.gameboard[x][y] === 0) {
+      this.gameboard[x][y] = "X";
     }
     this.counter++;
   }
 
   checkShipSunk() {
     this.ships.forEach((ship) => {
-      if (ship.timesHit === ship.shipLength()){
+      if (ship.timesHit === ship.shipLength()) {
         ship.isSunk = true;
       }
-      if(ship.isSunk){
+      if (ship.isSunk) {
         console.log("Du hast " + ship.name + " zum sinken gebracht");
       }
     });
   }
 
-  checkIfShipIsThere(){
-    if(this.gameboard[x][y] != ship.shipNumber){
+  checkIfShipIsThere() {
+    if (this.gameboard[x][y] != ship.shipNumber) {
       this.placeShip(ship, x, y);
     }
   }
