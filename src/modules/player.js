d99bb0ddfab12ba47604a31d13baea303d807a5a
vlGoku <markolucic21@outlook.de>
2024-01-18 19:40:37 +0100

    add attackEnemy()

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,8 +1,24 @@
 "use strict";
 
+import { Gameboard } from "./gameboard";
+
 class Player {
   constructor(name) {
     this.name = name;
+    this.enemyGameboard = new Gameboard();
+  }
+
+  attackEnemy(x, y) {
+    const result = this.enemyGameboard.attackShip(x, y);
+    if (result === "Treffer") {
+      console.log("Treffer!");
+    } else if (result === "X") {
+      console.log("Bereits an dieser Stelle geschossen!");
+    } else if (result === "Versenkt") {
+      console.log("Versenkt!");
+    } else {
+      console.log("Verfehlt!");
+    }
   }
 }
 
